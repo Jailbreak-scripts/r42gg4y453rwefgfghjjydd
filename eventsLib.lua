9c7f1eb67eecdd0463ee8666675d45c626e4c7cb
TimLiz <89855787672@mail.ru>
2022-09-29 21:29:16 +0300

    Create eventsLib.lua

---
+++ eventsLib.lua
@@ -0,0 +1,35 @@
+local LocalScript = game:GetService("Players").LocalPlayer.PlayerScripts.LocalScript
+function ghdf3292ngf923o13400jfggh984()
+    print("Events lib by MrWorst#0856 | Version: 29.09.2022 MINIFED")
+    local aux = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Upbolt/Hydroxide/revision/ohaux.lua"))()
+    local mainEvent, buyEvent
+
+    function mainEvent()
+        local scriptPath = LocalScript
+        local closureName = "EventFireServer"
+        local upvalueIndex = 2
+        local closureConstants = {}
+        local closure = aux.searchClosure(scriptPath, closureName, upvalueIndex, closureConstants)
+        return(debug.getupvalues(closure)[2])
+    end
+
+    --29.09.2022
+    function buyEvent()
+        local scriptPath = LocalScript
+        local closureName = "aoojkao0"
+        local upvalueIndex = 2
+        local closureConstants = {
+            [1] = "hems"
+        }
+        local closure = aux.searchClosure(scriptPath, closureName, upvalueIndex, closureConstants)
+        local elementIndex = "ac0alb5j"
+        return(debug.getupvalue(closure, upvalueIndex)[elementIndex])
+    end
+
+    return {
+        ["main"] = mainEvent(),
+        ["buy"] = buyEvent()
+    }
+end
+
+return ghdf3292ngf923o13400jfggh984()
