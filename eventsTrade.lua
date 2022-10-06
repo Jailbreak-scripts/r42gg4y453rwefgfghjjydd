43af5c38048d262813992fc8cc686bd8c8ebf5c1
TimLiz <89855787672@mail.ru>
2022-10-06 16:45:39 +0300

    Update eventsTrade.lua

--- eventsTrade.lua
+++ eventsTrade.lua
@@ -1,6 +1,6 @@
 local LocalScript = game:GetService("Players").LocalPlayer.PlayerScripts.LocalScript
 function ghdf3292ngf923o13400jfggh984()
-    print("Events lib by MrWorst#0856 | Version: 1.10.2022 MINIFED")
+    print("Events lib by MrWorst#0856 | Version: 6.10.2022 MINIFED")
     local aux = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Upbolt/Hydroxide/revision/ohaux.lua"))()
     local mainEvent, buyEvent
 
@@ -13,16 +13,16 @@ function ghdf3292ngf923o13400jfggh984()
         return(debug.getupvalues(closure)[2])
     end
 
-    --1.10.2022
+    --6.10.2022
     function buyEvent()
         local scriptPath = LocalScript
-        local closureName = "fpzuxfp0"
+        local closureName = "jdqkixoy"
         local upvalueIndex = 2
         local closureConstants = {
             [1] = "hems"
         }
         local closure = aux.searchClosure(scriptPath, closureName, upvalueIndex, closureConstants)
-        local elementIndex = "k85om4ks"
+        local elementIndex = "y9wf18nr"
         return(debug.getupvalue(closure, upvalueIndex)[elementIndex])
     end
 
