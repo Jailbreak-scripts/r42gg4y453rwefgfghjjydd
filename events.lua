e9a0122bc7efd16d4b0ef6a210efb50a5e395d87
TimLiz <89855787672@mail.ru>
2022-10-30 00:20:30 +0300

    Bumped

--- events.lua
+++ events.lua
@@ -9,7 +9,7 @@ _localPlayer_ = __players__.LocalPlayer or __players__.PlayerAdded:Wait()
 
 local LocalScript = _localPlayer_:WaitForChild("PlayerScripts", 15):WaitForChild("LocalScript", 15)
 function ghdf3292ngf923o13400jfggh984()
-    print("Events lib by TimLiz#2952 | Version: 28.10.2022")
+    print("Events lib by TimLiz#2952 | Version: 30.10.2022")
 
     local aux = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Upbolt/Hydroxide/revision/ohaux.lua"))()
     local mainEvent, moneyEvent, teamEvent, carEnterEvent, carExitEvent
@@ -23,7 +23,7 @@ function ghdf3292ngf923o13400jfggh984()
         return(debug.getupvalues(closure)[2])
     end
 
-    --28.10.2022
+    --30.10.2022
     function moneyEvent()
         local scriptPath = LocalScript
         local closureName = "EventFireServer"
@@ -33,7 +33,7 @@ function ghdf3292ngf923o13400jfggh984()
         return(debug.getupvalue(closure, upvalueIndex)[elementIndex])
     end
 
-    --28.10.2022
+    --30.10.2022
     function teamEvent()
         local scriptPath = LocalScript
         local closureName = "EventFireServer"
@@ -43,7 +43,7 @@ function ghdf3292ngf923o13400jfggh984()
         return(debug.getupvalue(closure, upvalueIndex)[elementIndex])
     end
 
-    --28.10.2022
+    --30.10.2022
     function carEnterEvent()
         local scriptPath = LocalScript
         local closureName = "EventFireServer"
@@ -53,7 +53,7 @@ function ghdf3292ngf923o13400jfggh984()
         return(debug.getupvalue(closure, upvalueIndex)[elementIndex])
     end
 
-    --28.10.2022
+    --30.10.2022
     function carExitEvent()
         local scriptPath = LocalScript
         local closureName = "EventFireServer"
